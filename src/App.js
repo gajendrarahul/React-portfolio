6a47b2f44377d03239d8dcfac044da54c2094ba5
gajendrarahul <mahatogajen123@gmail.com>
2021-03-19 22:21:07 +0545

    Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+import Particles from 'react-particles-js';
 import NavBar from "./components/Navbar";
+import Header from "./components/Header";
 
 function App() {
   return (
+    <>
+    <Particles
+    className="particles-canvas"
+    params = {{
+      particles: {
+        number: {
+          value:30,
+          density:{
+            enable:true,
+            value_area:900
+          }
+        },
+        shape:{
+          type: 'circle',
+          stroke: {
+             width:6,
+             color: '#f9ab00',
+
+          }
+        }
+      }
+    }}
+    />
     <NavBar/>
+    <Header/>
+    </>
   );
 }
 
